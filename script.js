7e373b28aa4be9c132485c5b47011557cfd6c264
Columbus Utrigas <ColumbusUtrigas@gmail.com>
2021-10-19 17:41:34 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+function appendHtml(el, str) {
+    var div = document.createElement('div'); //container to append to
+    div.innerHTML = str;
+    while (div.children.length > 0) {
+        el.appendChild(div.children[0]);
+    }
+}
+
+function appendCard(to, header, tags, desc, link, linktext) {
+    var html = `
+    <div class="work">
+        <h3 class="project-header">${header}</h3>
+        <h3 class="project-header language">${tags}</h3>
+        <p>${desc}
+        </p>
+        <a href="${link}" class="link">
+            ${linktext}
+            <div class="link-arrow">
+                <svg width="21" height="10" viewBox="0 0 21 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.6667 1L19.6667 5M19.6667 5L15.6667 9M19.6667 5H0" stroke="#1E1E1E"/>
+                </svg>
+            </div>
+        </a>
+    </div>
+    `;
+    appendHtml(to, html);
+}
+
+function appendWork(to, info) {
+    appendCard(to, info.header, info.tags, info.description, info.link, "github");
+}
+
+function appendArticle(to) {
+    appendCard(to, "Article", "Tag, tag, tag", "Some short description", "", "read");
+}
+
+function loadWorks() {
+    const columnsNum = 3;
+
+    let num = Math.trunc(worksJson.length / columnsNum);
+    let remains = worksJson.length % columnsNum;
+
+    let counter = 0;
+    for (let col = 1; col <= columnsNum; col++)
+    {
+        let column = document.getElementById(`works-column-${col}`)
+
+        for (let i = 0; i < num; i++) {
+            appendWork(column, worksJson[counter++])
+        }
+
+        if (remains) {
+            appendWork(column, worksJson[counter++]);
+            remains--;
+        }
+    }
+}
+
+function loadBlog() {
+    let column = document.getElementById(`blog-column-${1}`)
+
+    appendArticle(column);
+}
+
 window.onload = function() {
     let arrow = document.getElementById("arrow");
     let top = document.getElementById("top");
@@ -6,11 +70,6 @@ window.onload = function() {
         window.scrollTo(0, top.scrollHeight);
     });
 
-    // request to visits counter server
-    const url = "https://cutrigasvisitscounter.space";
-    fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        cache: 'no-cache'
-    });
+    loadWorks();
+    loadBlog();
 }
---
+++ works.js
@@ -0,0 +1,44 @@
+var worksJson = [
+    {
+        "header": "Columbus Engine",
+        "tags": "C++",
+        "description": "Crossplatform 3D engine. It using OpenGL (soon D3D and Vulkan) for rendering, have dynamic lighting, PBR, flexible particle simulation, rigidbody physics, advanced math library, component-system.",
+        "link": "https://github.com/ColumbusUtrigas/ColumbusEngine"
+    },
+    {
+        "header": "Columbus CMF",
+        "tags": "C++",
+        "description": "3D model format, add-on for Blender, C library for loading/saving and console util.",
+        "link": "https://github.com/ColumbusUtrigas/ColumbusCMF"
+    },
+    {
+        "header": "Columbus Raytracer",
+        "tags": "C++",
+        "description": "Simple CPU raytracer.",
+        "link": "https://github.com/ColumbusUtrigas/ColumbusRaytracer"
+    },
+    {
+        "header": "cuserial",
+        "tags": "C",
+        "description": "C header-only library for working with serial port.",
+        "link": "https://github.com/ColumbusUtrigas/cuserial"
+    },
+    {
+        "header": "corinfo",
+        "tags": "C",
+        "description": "C one-file header-only library for getting system information like size of RAM, CPU cores count and other.",
+        "link": "https://github.com/ColumbusUtrigas/corinfo"
+    },
+    {
+        "header": "Columbus Image",
+        "tags": "C",
+        "description": "Simple C library for loading/saving PNG, JPG, BMP, TGA, TIF images.",
+        "link": "https://github.com/ColumbusUtrigas/ColumbusImage"
+    },
+    {
+        "header": "cuenc",
+        "tags": "C",
+        "description": "Small library which realizing hash, encryption and decryption algorithms.",
+        "link": "https://github.com/ColumbusUtrigas/cuenc"
+    }
+]
